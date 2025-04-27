5e8463e6673ecc86973e52883bb6df6dd46a03fc
jaylin <jaylin99032@gmail.com>
2025-04-28 01:00:07 +0800

    加入搜尋有幾頁

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+from tqdm import tqdm
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
@@ -34,7 +37,9 @@ def parse_content(content):
 
         title = content[0].find("a", class_="title").text
         price = content[0].find("span", class_="price").text
-        area = content[0].find("span", class_="area").text
+        # area = content[0].find("span", class_="area").text
+        areas = content[0].find_all("span", class_="area")
+        area = areas[len(areas)-1].text
         address = content[0].find("span", class_="address").text
         url = content[0].find("a", class_="title")["href"]
 
@@ -58,7 +63,7 @@ def get_content(url):
 
     response = requests.get(url, headers=headers, cookies=cookie, verify=False)
 
-    print("content got: ", response.status_code)
+    # print("content got: ", response.status_code)
     return response.text
 
 
@@ -66,8 +71,22 @@ def get_urls():
 
     urls = []
 
-    for i in range(0, 10):
-        if i == 0:
+    # get total pages
+
+    first_page = requests.get("https://rent.591.com.tw/list?region=4", headers=headers, cookies=cookie, verify=False)
+    soup = BeautifulSoup(first_page.text, "html.parser")
+
+    links = soup.find_all("a")
+    filtered_links = filter(lambda link: "/list?region=4" in link["href"], links)
+    filtered_links = list(filtered_links)
+    print("filtered_links: ", filtered_links[-2].text)
+
+    total_pages = int(filtered_links[-2].text)
+
+    # for i in range(1, total_pages+1):
+    for i in range(1, 10):
+
+        if i == 1:
             urls.append("https://rent.591.com.tw/list?region=4")
         else:
             urls.append("https://rent.591.com.tw/list?region=4&page=" + str(i))
@@ -79,7 +98,7 @@ if __name__ == "__main__":
     # get url
     urls = get_urls()
 
-    for url in urls:
+    for url in tqdm(urls):
         # print("get url: ", url)
         # get content
         content = get_content(url)
@@ -88,10 +107,10 @@ if __name__ == "__main__":
         results = parse_content(content)
         # print("parse content")
 
-        for result in results:
-            print("title: ", result["title"])
-            print("price: ", result["price"])
-            print("area: ", result["area"])
-            print("address: ", result["address"])
-            print("url: ", result["url"])
-            print("=====================================")
+        # for result in results:
+        #     print("title: ", result["title"])
+        #     print("price: ", result["price"])
+        #     print("area: ", result["area"])
+        #     print("address: ", result["address"])
+        #     print("url: ", result["url"])
+        #     print("=====================================")
